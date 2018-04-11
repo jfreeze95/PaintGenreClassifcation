af7a66288239fd779513aa36f549e50c108e6cd8
jfreeze95 <37780744+jfreeze95@users.noreply.github.com>
2018-04-11 16:32:14 -0400

    image resize script

---
+++ imageresize.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from PIL import Image
+import re
+from resizeimage import resizeimage
+
+# f = open('train_files1.csv', 'r')
+
+
+# for line in f:
+# 	line = line.strip()
+# 	line = re.sub('\t','',line)
+# 	columns = line.split(',')
+# 	img = 'train/'+columns[0]
+# 	im = Image.open(img)
+# 	print(columns[0])
+# 	cover = im.resize((750, 750), Image.NEAREST)
+# 	cover.save(img, im.format)
+# f.close()
+
+f = open('test_files1.csv', 'r')
+
+
+for line in f:
+	line = line.strip()
+	line = re.sub('\t','',line)
+	columns = line.split(',')
+	img = 'test/'+columns[0]
+	im = Image.open(img)
+	print(columns[0])
+	cover = im.resize((750, 750), Image.NEAREST)
+	cover.save(img, im.format)
+f.close()
